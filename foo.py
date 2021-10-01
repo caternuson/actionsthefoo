5c91f0b994de8329c6c8075e9fa23b22d7a040ac
caternuson <methuzla@speakeasy.net>
2021-10-01 10:15:45 -0700

    update

--- foo.py
+++ foo.py
@@ -2,6 +2,7 @@ import subprocess
 
 print("Here's what changed:")
 
-res = subprocess.getoutput('git diff --name_only HEAD^ HEAD')
+#res = subprocess.getoutput('git diff --name_only HEAD^ HEAD')
+res = subprocess.getoutput('git log')
 
 print(res)
