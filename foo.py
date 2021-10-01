21a7205c9d7da5fb1023c56b0d085c3039edfff1
caternuson <methuzla@speakeasy.net>
2021-10-01 11:27:55 -0700

    pr2

--- foo.py
+++ foo.py
@@ -1,7 +1,17 @@
 import subprocess
 
+all_changes = subprocess.getoutput('git diff --name-only origin/main').split('\n')
+
 print("Here's what changed:")
+print(all_changes)
 
-res = subprocess.getoutput('git diff --name-only origin/main')
+changes_to_test = []
+print("Looking for subfolders...")
+for path in all_changes:
+    if path.startswith('.') or not "/" in path:
+        print("  ignoring: ", path)
+        continue
+    print("  found: ", path)
+    changes_to_test.append(path)
 
-print(res)
+print(changes_to_test)
